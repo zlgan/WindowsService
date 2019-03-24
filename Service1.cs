3285273b4776d9e825a1ef8a1b7efa400d4b14e9
zlgan <278408588@qq.com>
2019-03-24 10:00:31 +0800

    first commit

---
+++ Service1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        MessageQueue messageQueue = null;
+        public Service1()
+        {
+            InitializeComponent();
+
+            System.Timers.Timer tm = new System.Timers.Timer();
+            tm.Elapsed += Tm_Elapsed;
+            tm.Interval = 5000;
+            tm.Enabled = true;
+
+            try
+            {
+                if (MessageQueue.Exists(@".\Private$\MyQueues"))
+                {
+                    messageQueue = new MessageQueue(@".\Private$\MyQueues");
+                }
+                else
+                {
+                    messageQueue = MessageQueue.Create(@".\Private$\MyQueues");
+                    messageQueue.Label = "Newly Created Queue";
+                }
+            }
+            catch (Exception ex)
+            {
+                this.WriteLog(ex.ToString());
+            }
+            
+        }
+
+        private void Tm_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+            try
+            {
+                Message msg = messageQueue.Receive();
+                msg.Formatter = new System.Messaging.XmlMessageFormatter(new Type[] { typeof(string) });
+                string sql = string.Format("insert into TestLog values('{0}','{1}')", DateTime.Now.ToString(), msg.Body.ToString());
+                DBUtil.SqlExecute(sql);
+            }
+            catch (Exception ex)
+            {
+                this.WriteLog(ex.ToString());
+            }
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            this.WriteLog("我的服务启动");
+        }
+
+        protected override void OnStop()
+        {
+            this.WriteLog("我的服务器停止");
+        }
+
+        #region 记录日志
+        /// <summary>
+        /// 记录日志
+        /// </summary>
+        /// <param name="msg"></param>
+        private void WriteLog(string msg)
+        {
+
+            //string path = @"C:\log.txt";
+
+            //该日志文件会存在windows服务程序目录下
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\log.txt";
+            FileInfo file = new FileInfo(path);
+            if (!file.Exists)
+            {
+                FileStream fs;
+                fs = File.Create(path);
+                fs.Close();
+            }
+
+            using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + "   " + msg);
+                }
+            }
+        }
+        #endregion
+}
+}
---
+++ dbUtil.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WindowsService1
+{
+    /// <summary>
+    /// 数据操作工具类
+    /// 作者：老郑
+    /// 日期：2016年7月25日
+    /// 版本：v1.1
+    /// </summary>
+    public class DBUtil
+    {
+
+        /// <summary>
+        /// 获取数据库连接
+        /// </summary>
+        /// <returns>返回一个新的数据库连接</returns>
+        public static SqlConnection GetSqlConnection()
+        {
+            string source = System.Configuration.ConfigurationManager.ConnectionStrings["myConn"].ConnectionString;
+            return new SqlConnection(source);
+        }
+
+        /// <summary>
+        /// 执行SQL语句 - 为了保证数据的完整性开启了事务
+        /// </summary>
+        /// <param name="sql">SQL语句（多条SQL语句可以用分号隔开）</param>
+        /// <returns>返回受影响的行数</returns>
+        public static int SqlExecute(string sql)
+        {
+            try
+            {
+                using (SqlConnection conn = GetSqlConnection())
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    int result = cmd.ExecuteNonQuery();
+                    return result;
+                }
+            }
+            catch (SqlException e)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// DataReader 方式读取数据
+        /// </summary>
+        /// <param name="sql">执行的SQL</param>
+        public static void Sql2DataReader(string sql)
+        {
+            try
+            {
+                using (SqlConnection conn = GetSqlConnection())
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("select top 10 * from Base_Area", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        // TODO 自己的代码
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 查询出DataSet
+        /// </summary>
+        /// <param name="sql">查询的SQL语句</param>
+        /// <returns></returns>
+        public static DataSet Sql2DataSet(string sql)
+        {
+            try
+            {
+                SqlDataAdapter da = null;
+                DataSet ds = new DataSet();
+                using (SqlConnection conn = GetSqlConnection())
+                {
+                    conn.Open();
+                    da = new SqlDataAdapter(sql, conn);
+                    da.Fill(ds);
+                    return ds;
+                }
+            }
+            catch (SqlException e)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 查询并返回DataTable
+        /// </summary>
+        /// <param name="sql">查询的SQL语句</param>
+        /// <returns></returns>
+        public static DataTable Sql2DataTable(string sql)
+        {
+            return Sql2DataSet(sql).Tables[0];
+        }
+    }
+}
